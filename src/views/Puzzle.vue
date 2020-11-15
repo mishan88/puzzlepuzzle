06881d40b30bce6e2cf7d27dd31805f03e812039
mishan88 <mishanhideaki88@gmail.com>
2020-11-15 21:24:32 +0900

    add: Puzzle component

---
+++ src/views/Puzzle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <PanelPlace />
+    <PanelList />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import PanelList from '@/components/PanelList.vue'
+import PanelPlace from '@/components/PanelPlace.vue'
+
+export default defineComponent({
+  name: 'Puzzle',
+  components: {
+    PanelPlace,
+    PanelList
+  }
+})
+
+</script>
