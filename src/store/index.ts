ffd9ee5f876eb0d84df67c9e409167d12c137a64
mishan88 <mishanhideaki88@gmail.com>
2020-11-15 21:24:44 +0900

    add: vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,8 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    panels: [
+      { id: 1, asset: 'https://picsum.photos/id/11/10/6' },
+      { id: 2, asset: 'https://picsum.photos/id/11/500/300' }
+    ],
+    panelQueue: null
+  },
+  getters: {
   },
   mutations: {
+    addPanel (state, payload) {
+      state.panels.push(payload)
+    },
+    addPanelQueue (state, payload) {
+      state.panelQueue = payload
+    },
+    deletePanelQueue (state) {
+      state.panelQueue = null
+    }
   },
   actions: {
   },
