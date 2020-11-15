42684df7a00079bfad7f654812c73e3213c349b1
mishan88 <mishanhideaki88@gmail.com>
2020-11-15 23:17:38 +0900

    add: file upload

--- src/components/PanelList.vue
+++ src/components/PanelList.vue
@@ -1,33 +1,37 @@
 <template>
   <div>
-    <v-file-input label="File input" ref="fileInputRef" v-on:change="addPanelCandidate"></v-file-input>
-    <PanelListItem v-for="panelCandidate in panelCandidates" v-bind:id="panelCandidate.id" v-bind:asset="panelCandidate.asset" :key="panelCandidate.id" />
+    <v-file-input multiple accept="image/*" label="File input" ref="fileInputRef" v-on:change="addPanelCandidate"></v-file-input>
+    <div v-if="!panelCandidates">
+      Add Panel!
+    </div>
+    <div v-else>
+      <PanelListItem v-for="panelCandidate in panelCandidates" v-bind:id="panelCandidate.id" v-bind:asset="panelCandidate.asset" :key="panelCandidate.id" />
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, computed } from '@vue/composition-api'
 import PanelListItem from '@/components/PanelListItem.vue'
 
 export default defineComponent({
   name: 'PanelList',
   setup (props, { root }) {
-    function addPanelCandidate (payloads: Blob[]) {
-      const reader = new FileReader()
-      reader.addEventListener('load', () => {
-        console.log('hoge')
-      })
-      for (const payload of payloads) {
-        root.$store.commit('addPanel', payload)
+    function addPanelCandidate (files: File[]) {
+      if (event !== null) {
+        let nextId = root.$store.state.panels.length
+
+        for (const file of files) {
+          const src = URL.createObjectURL(file)
+          root.$store.commit('addPanel', { id: nextId++, asset: src })
+        }
       }
     }
-
+    const panelCandidates = computed(() => {
+      return root.$store.state.panels
+    })
     return {
-      addPanelCandidate
-    }
-  },
-  computed: {
-    panelCandidates () {
-      return this.$store.state.panels
+      addPanelCandidate,
+      panelCandidates
     }
   },
   components: {
