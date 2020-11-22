81a92e53a34c8ebb3c194dbd30e0f267efacc92c
mishan88 <mishanhideaki88@gmail.com>
2020-11-23 08:28:35 +0900

    fix: initial state

--- src/store/modules/panelclear.ts
+++ src/store/modules/panelclear.ts
@@ -2,7 +2,7 @@ import { Module, VuexModule, Mutation } from 'vuex-module-decorators'
 
 @Module({ namespaced: true, name: 'panelClear' })
 export default class PanelClearModule extends VuexModule {
-  panelClear = true
+  panelClear = false
 
   @Mutation
   clear () {
